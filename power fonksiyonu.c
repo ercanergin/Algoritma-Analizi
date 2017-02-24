3a3aede1d4fba5f1a2b17f2a04e54791c85c0e13
Ercan ERGİN <ercanergin623@gmail.com>
2017-02-24 13:13:55 +0200

    Add files via upload

---
+++ power fonksiyonu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Kodun karmaþýklýðý O(n)'dir.
+int power(int a,int b){
+    int toplam=1;
+    for(int i=0;i<b;i++){
+        toplam+=a;
+    }
+
+}
+int main(){
+}
+
+
