96e7cf7b5b81d79f56cc37fe2bcf08e1bf8a886c
Ercan ERGİN <ercanergin623@gmail.com>
2017-03-03 12:53:15 +0200

    Update main.c

--- Hafta-2/main.c
+++ Hafta-2/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-// Bu algoritmanın karmaşıklığı logn dir.
 
 int x;
 unsigned int y;
+int sayac=0;
 
+// Bu algoritmanın karmaşıklığı logn dir.
 
 
 int usAl(int x, unsigned int y)
 {
+   
     int sonuc = 1;
     while (y > 0)
     {
@@ -17,18 +19,21 @@ int usAl(int x, unsigned int y)
             }
         y = y/2;
         x = x*x;
+        sayac++;
     }
     return sonuc;
 }
 
 int main(int argc, char *argv[])
 {
+    
     printf("X sayisini giriniz : ");
     scanf("%d",&x);
     printf("Y sayisini giriniz : ");
     scanf("%d",&y);
     int sonuc = usAl(x,y);
     printf("Sonuc : %d \n",sonuc);
+    printf("sayac=%d \n",sayac);
   
   system("PAUSE");	
   return 0;
