622c223542686531eef084e6f0978e43f2aa19bf
Ercan ERGİN <ercanergin623@gmail.com>
2017-03-03 12:40:56 +0200

    Add files via upload

---
+++ Hafta-2/mainn.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int us(int x,int y){  
+  int i,a=1;
+  for(i=1;i<=y;i++){ 
+     a=a*x;             
+} 
+return a;
+   
+}
+int main()
+{  
+    int x,y,sonuc;
+   
+  printf("\nUs alma ");
+  printf("\n x = ");
+  scanf("%d",&x);
+  printf("\n y = ");
+  scanf("%d",&y);   
+  sonuc=us(x,y);
+  printf("\n \n sonuc = %d ",sonuc); 
+   getch();
+   return 0;
+}
